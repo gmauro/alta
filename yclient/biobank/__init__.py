29ecd34ddbfb32b05b675282d1a26fdeeb9818e3
Gianmauro Cuccuru <gmauro@crs4.it>
2015-04-28 23:38:59 +0200

    handled the absence of OMERO's libraries

--- yclient/biobank/__init__.py
+++ yclient/biobank/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from bl.vl.kb import KnowledgeBase as KB
 from yclient.utils import a_logger
 
@@ -9,6 +11,11 @@ class BioBank(object):
         self.log = a_logger(self.__class__.__name__, level=loglevel)
         self.log.debug('Opening connection to {} OMERO server'.format(
             host))
-        self.kb = KB(driver='omero')(host, user, passwd)
+        try:
+            self.kb = KB(driver='omero')(host, user, passwd)
+        except ImportError, e:
+            self.log.error("{}. Have you forgotten to load the OMERO's "
+                           "libraries?".format(e))
+            sys.exit()
         self.log.info('Connected to {} OMERO server'.format(host))
         self.log.info('user: {}'.format(user))
