fd74a6daa7743b976d4cbdb7cf34c057094ac6b5
ratzeni <rossano.atzeni@crs4.it>
2015-07-27 15:57:23 +0200

    added a reg_file() method to register a local file in irods added a set_irods_metadata

--- alta/yrods/__init__.py
+++ alta/yrods/__init__.py
@@ -1,5 +1,6 @@
 import irods
 from alta.utils import a_logger
+import subprocess
 
 
 class yRODS(object):
@@ -35,6 +36,23 @@ class yRODS(object):
             metadata[m[0]] = {'value': m[1], 'units': m[2]}
         return metadata
 
+    def set_irods_metadata(self, path, metadata):
+        # FIXME: add path validation
+        if path.startswith(self.prefix):
+            path = path.replace(self.prefix, '')
+
+        for name, value in metadata.iteritems():
+            self._set_irods_metadata(path,
+                                     name=name,
+                                     value=value.get('value', None),
+                                     units=value.get('units', ''))
+
+    def reg_file(self, local_path, path):
+        # FIXME: add path validation
+        if path.startswith(self.prefix):
+            path = path.replace(self.prefix, '')
+        self.__irods_check_output(['ireg', local_path, path])
+
     def get_irods_file(self, path):
         # FIXME: add path validation
         if path.startswith(self.prefix):
@@ -42,3 +60,44 @@ class yRODS(object):
 
         f = irods.irodsOpen(self.conn, path, 'r')
         return f
+
+    def _set_irods_metadata(self, path, name, value, units=''):
+        # FIXME: add path validation
+        if path.startswith(self.prefix):
+            path = path.replace(self.prefix, '')
+
+        if irods.irodsOpen(self.conn, path, 'r'):
+            irods.addFileUserMetadata(self.conn, path,
+                                      name=name,
+                                      value=value,
+                                      units=units)
+
+    def __irods_check_output(self, args):
+        try:
+            output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+            return output
+        except subprocess.CalledProcessError as e:
+            raise IRodsError(e.cmd, e.returncode, e.output)
+
+
+class IRodsError(RuntimeError):
+    def __init__(self, cmd, retcode, output=None):
+        self._cmd = cmd
+        self._retcode = retcode
+        self._output = output
+        cmdstring = ' '.join(map(str, self._cmd))
+        message = "iRODS error -> cmd: %s; returncode: %s; output: %s" % \
+                (cmdstring, self._retcode, self._output)
+        RuntimeError.__init__(self, message)
+
+    @property
+    def output(self):
+        return self._output
+
+    @property
+    def retcode(self):
+        return self._retcode
+
+    @property
+    def cmd(self):
+        return self._cmd
